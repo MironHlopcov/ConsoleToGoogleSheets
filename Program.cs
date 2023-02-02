e79c8ea5dbcbf11ce2544499b81dd31cd7a5e999
miron <miron>
2023-02-02 14:06:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleToGoogleSheets;
+
+Console.WriteLine("Hello, World!");
+SheetsAccess sheetsAccess = new SheetsAccess();
+
+sheetsAccess.ReadEntrys();
+sheetsAccess.CreateEntry();
+
+
